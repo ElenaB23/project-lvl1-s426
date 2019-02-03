17929b24c5f92190079fce181e9dfde1fabd4054
vagrant <vagrant@m1>
2019-02-03 21:02:44 +0000

    Add game IsPrime

---
+++ src/Games/IsPrime.php
@@ -0,0 +1,27 @@
+<?php
+namespace BrainGames\Games\IsPrime;
+
+use function BrainGames\Engine\gameEngine;
+
+const RULE = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+
+function run()
+{
+    $getData = function () {
+        $question = rand(1, 100);
+        $rightAnswer = isPrime($question) ? 'yes' : 'no';
+        return [$question, $rightAnswer];
+    };
+    gameEngine(RULE, $getData);
+}
+
+function isPrime($number)
+{
+    if ($number < 2) return false;
+    for ($divisor = 2; $divisor <= ceil($number / 2); $divisor++) {
+        if ($number % $divisor === 0) {
+            return false;
+        }
+    }
+    return true;
+}
